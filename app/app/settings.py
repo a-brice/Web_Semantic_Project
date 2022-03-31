5b8d1c794a9ad53388d29ef39454107bea444372
Brice <akouvibrice@gmail.com>
2022-03-31 20:36:40 +0200

    secret key securisation

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('web_sem_pwd')
+SECRET_KEY = os.getenv('SECRET_KEY', '8vb(5$*u9_xx3#3!=#*zgebjanm7wpqq6^8$c!nqirgh%$h5w!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
